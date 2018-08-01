3c4b9b6cb8cbf9236a44d3826bdd309499c386e4
Juan Tenezaca <tenj34@gmail.com>
2018-08-01 21:00:22 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe
 
 
+=begin
   def initialize(brand)
     @brand = brand
   end
@@ -8,5 +9,11 @@ class Shoe
   def brand
     @brand
   end
+=end
+attr_accessor :color, :size, :material, :condition
+attr_reader :brand 
+
+
+
 
 end
